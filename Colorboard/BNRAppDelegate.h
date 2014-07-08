82f860b012bc9566d355249fd8fbdee56121755e
William Kong <riverw09@gmail.com>
2014-07-08 09:02:54 -0400

    Initial Commit

---
+++ Colorboard/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Colorboard
+//
+//  Created by William Kong on 2014-07-08.
+//  Copyright (c) 2014 William Kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Colorboard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Colorboard
+//
+//  Created by William Kong on 2014-07-08.
+//  Copyright (c) 2014 William Kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
